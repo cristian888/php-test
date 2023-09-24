7b6f6b69a41d2114795e84abf126bdb321d8989b
cristian888 <venollo01@gmail.com>
2023-09-24 10:49:43 +0100

    new variables

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
 <?php
 
-$favcolor = "red";
+$first_name = "fifi";
+$last_name = "${first_name} Gigi";
 
-switch ($favcolor) {
-    case "green_2":
-        echo "your favourite color is green_2";
-        break;
-    case "red":
-        echo "Your favorite color is red!";
-        break;
-    case "blue":
-        echo "Your favorite color is blue!";
-        break;
-    case "green":
-        echo "Your favorite color is green!";
-        break;
-    default:
-        echo "Your favorite color is neither red, blue, nor green!";
-}
+echo $first_name;
+
+echo "<br>";
 
