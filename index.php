97a07291a65dd6f5a8722c899f9f91c9ae6676b7
cristian888 <venollo01@gmail.com>
2023-09-24 09:46:51 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$favcolor = "red";
+
+switch ($favcolor) {
+    case "green_2":
+        echo "your favourite color is green_2";
+        break;
+    case "red":
+        echo "Your favorite color is red!";
+        break;
+    case "blue":
+        echo "Your favorite color is blue!";
+        break;
+    case "green":
+        echo "Your favorite color is green!";
+        break;
+    default:
+        echo "Your favorite color is neither red, blue, nor green!";
+}
+
