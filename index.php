59bdc1cc0cf738096025351c92984edd9b2deb6d
cristian888 <venollo01@gmail.com>
2023-09-26 09:19:30 +0100

    changes 33

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
 <?php
 
-$x = 5;
-$y = 10;
-//heredoc
-$text = <<<TEXT
-    Line 1 = $x
-    Line 2 = $y
-TEXT;
+function getPosts(): array
+{
+    $posts = [
+        [
+            'title' => 'How to learn PHP',
+            'content' => 'This is how you learn PHP.',
+        ],
+        [
+            'title' => 'How to learn MySQL',
+            'content' => 'This is how you learn MySQL.',
+        ],
+        [
+            'title' => 'How to learn Nginx',
+            'content' => 'This is how you learn Nginx.',
+        ],
+    ];
+    return $posts;
+}
 
-echo nl2br($text);
+function getPostText (int $numPosts): string
+{
+    return $numPosts === 1 ? 'post' : 'posts';
+}
+
+$title = 'My Blog';
+//define variable $posts as an array
+$posts = getPosts();
+$numPosts = count($posts);
+$postText = getPostText($numPosts);
+$numPostsDisplay = "$numPosts $postText";
+?>
+<h1><?= $title ?></h1>
+<h2><?= $numPostsDisplay ?></h2>
+<?php for ($i = 0; $i < $numPosts; $i++) : ?>
+    <h3><?= $posts[$i]['title'] ?></h3>
+    <p><?= $posts[$i]['content'] ?></p>
+<?php endfor ?>
